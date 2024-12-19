859a368722ca00a3e40d3c1e2205b88f0fd95253
EyoabA1 <eyoabaddisu123@gmail.com>
2024-12-19 23:59:13 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Welcome to the Elite 101 Chatbot! ")
+name = input("May I get you name? ")
+age = input("Hello " + name + " how old are you?")
+choices = input("Please pick a number to select what you want!\n1. Place Holder\n2. Place Holder\n3. Place Holder\n4. Exit. ")
+if choices == "4":
+    print("Thank you for your time!") 
+elif choices == "1":
+    print("We are working on this choice come back a later time")
+elif choices == "2":
+    print("We are working on this choice come back a later time")
+elif choices == "3":
+    print("We are working on this choice come back a later time")
