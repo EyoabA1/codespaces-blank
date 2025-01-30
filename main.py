3e1422005a09602770a56e76c505a1fd8b48d59e
EyoabA1 <eyoabaddisu123@gmail.com>
2025-01-30 17:52:32 -0600

    Update main.py
    1/30/2025

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-print("Welcome to the Elite 101 Chatbot! ")
+print("Welcome to the Elite 101 Online Clothing Retailer ChatBot")
 name = input("May I get you name? ")
 age = input("Hello " + name + " how old are you?")
-choices = input("Please pick a number to select what you want!\n1. Place Holder\n2. Place Holder\n3. Place Holder\n4. Exit. ")
+choices = input("Please pick a number to select what you want!\n1. Check items prices and stock in store.\n2. Return your purchased items.\n3. Purchase items \n4. Exit. ")
 if choices == "4":
     print("Thank you for your time!") 
 elif choices == "1":
